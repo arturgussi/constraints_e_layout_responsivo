52eb367678dba982a6e5ee5386b3ae35b9279f5d
Artur Gussi <arturgussi@gmail.com>
2023-09-12 19:06:44 -0300

    Aula 2: Mergulhar em constraints

--- 2818-layouts-responsivos-main/lib/screens/highlights.dart
+++ 2818-layouts-responsivos-main/lib/screens/highlights.dart
@@ -9,18 +9,39 @@ class Highlights extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: HighlightItem(
-              imageURI: items[index]['image'],
-              itemTitle: items[index]['name'],
-              itemPrice: items[index]['price'],
-              itemDescription: items[index]['description']),
-        );
-      },
-      itemCount: items.length,
-    );
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+        child: CustomScrollView(
+          slivers: <Widget>[
+            const SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  bottom: 16.0,
+                ),
+                child: Text(
+                  'Destaques',
+                  style: TextStyle(
+                    fontFamily: 'Caveat',
+                    fontSize: 32,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return HighlightItem(
+                    imageURI: items[index]['image'],
+                    itemTitle: items[index]['name'],
+                    itemPrice: items[index]['price'],
+                    itemDescription: items[index]['description'],
+                  );
+                },
+                childCount: items.length,
+              ),
+            )
+          ],
+        ));
   }
 }
